469948f21e85121dd757c7dd3ffd47309d79b990
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2023-11-07 19:58:14 +0200

    Remade Lab1T1

--- Epic2Lab1Task1.cpp
+++ Epic2Lab1Task1.cpp
@@ -1,24 +1,61 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-void PrintTask(int a, double b)
+
+const float a1 = 1000;
+const float b1 = 0.0001;
+const double a2 = 1000;
+const double b2 = 0.0001;
+
+void PrintTask()
 {
-    cout << "a: " << a << endl;
-    cout << "b: " << b << endl;
+    cout << "-------------------------------" << endl;
+    cout << "a: 1000" << endl;
+    cout << "b: 0.0001" << endl;
+    cout << "-------------------------------" << endl;
     cout << "(a+b)^2-(a^2+2ab)" << endl;
     cout << "—————————————————" << endl;
     cout << "       b^2       " << endl;
+    cout << "-------------------------------" << endl;
 }
-double GetResult(int a, double b)
+
+void CountAndPrintFloatResult()
 {
-    double result = (pow(a+b, 2)-(pow(a, 2)+2*a*b))/pow(b, 2);
-    return result;
+    float a = pow(a1 + b1, 2);
+    cout << "First action: " << a << endl;
+    float b = pow(a1, 2);
+    cout << "Second action: " << b << endl;
+    float c = 2 * a1 * b1;
+    cout << "Third action: " << c << endl;
+    float d = a - (b + c);
+    cout << "Fourth action: " << d << endl;
+    float e = pow(b1, 2);
+    cout << "Fifth action: " << e << endl;
+    float f = d / e;
+    cout << "Float result: " << f << endl;
+    cout << "-------------------------------" << endl;
 }
+
+void CountAndPrintDoubleResult()
+{
+    double a = pow(a2 + b2, 2);
+    cout << "First action: " << a << endl;
+    double b = pow(a2, 2);
+    cout << "Second action: " << b << endl;
+    double c = 2 * a2 * b2;
+    cout << "Third action: " << c << endl;
+    double d = a - (b + c);
+    cout << "Fourth action: " << d << endl;
+    double e = pow(b2, 2);
+    cout << "Fifth action: " << e << endl;
+    double f = d / e;
+    cout << "Double result: " << f << endl;
+    cout << "-------------------------------" << endl;
+}
+
 int main()
 {
-    const int a = 1000;
-    const double b = 0.0001;
-    double result;
-    PrintTask(a, b);
-    cout << "Result: " << round(GetResult(a, b)) << endl;
+    PrintTask();
+    CountAndPrintFloatResult();
+    CountAndPrintDoubleResult();
 }
