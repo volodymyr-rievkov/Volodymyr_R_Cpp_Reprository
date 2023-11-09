2f83b000c7419a8c59ec284d6925f10c3d1430df
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2023-11-09 21:39:17 +0200

    Added AlgtLab

---
+++ AlgtLab1Task1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    bool fail = false;
+    int H;
+    int M;
+    int h;
+    int m;
+    cin >> H;
+    cin >> M;
+    for (int i = 0; i < 3; i++)
+    {   
+        cin >> h;
+        cin >> m;
+        if(h != 0 && m != 0) 
+        {
+            fail = true;
+        }
+        H -= h;
+        M -= m;
+    }
+    if(fail)
+    {
+        cout << "NO";
+    } 
+    else
+    {
+        if(H > 0 && M > 0)
+        {
+            cout << "YES";
+        }
+        else
+        {
+            cout << "NO";
+        }
+    } 
+}
