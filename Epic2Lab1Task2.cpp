07789c1e86f4854919eb4ca6cae95010823330d2
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2023-11-09 19:16:16 +0200

    CorrectedTasts

--- Epic2Lab1Task2.cpp
+++ Epic2Lab1Task2.cpp
@@ -2,20 +2,13 @@
 using namespace std;
 int main()
 {
-    double n1;
-    double m1;
+    int n;
+    int m;
     cout << "Enter n: ";
-    cin >> n1;
+    cin >> n;
     cout << "Enter m: ";
-    cin >> m1;
-    float n2 = n1;
-    float m2 = m1;
-    cout << "---------Double Result---------" << endl;
-    cout << "n+++m: " << n1+++m1 << endl;
-    cout << "n-->m: " << (n1-->m1) << endl;
-    cout << "m-->n: " << (m1-->n1) << endl;
-    cout << "---------Float Result---------" << endl;
-    cout << "n+++m: " << n2+++m2 << endl;
-    cout << "n-->m: " << (n2-->m2) << endl;
-    cout << "m-->n: " << (m2-->n2) << endl;
+    cin >> m;
+    cout << "n+++m: " << n+++m << endl;
+    cout << "n-->m: " << (n-->m) << endl;
+    cout << "m-->n: " << (m-->n) << endl;
 }
