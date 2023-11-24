eb7546bc451f787461d0bd1fef13fa28b52571c7
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2023-11-24 12:06:02 +0200

    EditedFolderCaption

---
+++ Epic 3/Practise2PatternsOutput/practise_task_2_volodymyr_rievkov.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int ChoosePattern()
+{
+    int action;
+    cout << " 1 - Square\n 2 - Rectangle\n 3 - Triangle\n 4 - Pyramid\n 5 - Diamond\nEnter number: ";
+    return action;
+}
+int main()
+{
+    ChoosePattern();
+}
