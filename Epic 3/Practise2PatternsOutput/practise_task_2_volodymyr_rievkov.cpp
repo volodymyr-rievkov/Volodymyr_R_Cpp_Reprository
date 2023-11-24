7e1b192bdff0e00f1c66e3f5a149b806b1a2fbf9
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2023-11-24 13:32:49 +0200

    EditedPatternOutput

--- Epic 3/Practise2PatternsOutput/practise_task_2_volodymyr_rievkov.cpp
+++ Epic 3/Practise2PatternsOutput/practise_task_2_volodymyr_rievkov.cpp
@@ -1,12 +1,77 @@
 #include<iostream>
+#include<vector>
+#include <functional>
 using namespace std;
-int ChoosePattern()
+void* PrintSquare()
 {
-    int action;
-    cout << " 1 - Square\n 2 - Rectangle\n 3 - Triangle\n 4 - Pyramid\n 5 - Diamond\nEnter number: ";
-    return action;
+    cout << "Triangle";
+    return nullptr;
 }
+void* PrintRectangle()
+{
+    
+    return nullptr;
+}
+void* PrintTriangle()
+{
+    
+    return nullptr;
+}
+void* PrintPyramid()
+{
+    
+    return nullptr;
+}
+void* PrintDiamond()
+{
+    
+    return nullptr;
+}
+void* Exit()
+{
+    return nullptr;
+}
+vector<void*> patterns {PrintSquare(), PrintRectangle(), PrintTriangle(), PrintPyramid(), PrintDiamond(), Exit()};
+// int ChoosePattern()
+// {
+//     int action;
+//     do
+//     {
+//         cout << " 1 - Square\n 2 - Rectangle\n 3 - Triangle\n 4 - Pyramid\n 5 - Diamond\n 6 - Exit\nEnter number: ";
+//         cin >> action;
+//     } while (action != 1 && action != 2 && action != 3 && action != 4 && action != 5 && action != 6);
+//     return action;
+// }
+// bool IsFilled()
+// {
+//     char choice;
+//     bool isFilled;
+//     do
+//     {
+//         cout << "Do you want to fill pattern(y/n): ";
+//         cin >> choice;
+//     } while (choice != 'y' && choice != 'n');
+//     if(choice == 'y')
+//     {
+//         isFilled = true;
+//     }
+//     else
+//     {
+//         isFilled = false;
+//     }
+//     return isFilled;
+// }
 int main()
 {
-    ChoosePattern();
+    patterns[0];
+    // switch(ChoosePattern())
+    // {
+    //     case 1 : 
+    //     case 2 :
+    //     case 3 :
+    //     case 4 :
+    //     case 5 :
+    //     case 6 : break;
+    // }
+    
 }
