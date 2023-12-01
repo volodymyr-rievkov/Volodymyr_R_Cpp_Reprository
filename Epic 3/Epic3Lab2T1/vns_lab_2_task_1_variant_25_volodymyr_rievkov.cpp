74f85cfa2312c7eb9643965a97eb86b599e51a33
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2023-12-01 15:29:00 +0200

    lab2 and lab 3 correction

--- Epic 3/Epic3Lab2T1/vns_lab_2_task_1_variant_25_volodymyr_rievkov.cpp
+++ Epic 3/Epic3Lab2T1/vns_lab_2_task_1_variant_25_volodymyr_rievkov.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+
 double sum = 0;
+
 double GetSum(int n)
 {
+
     if(n == 0)
     {
-        return 0;
+        return sum;
     }
-    sum += pow(n, 2)*exp(-sqrt(n));
-    GetSum(n-1);
-    return sum;
+    sum = pow(n, 2)*exp(-sqrt(n));
+    n--;
+    return sum + GetSum(n);
 }
+
 int main()
 {
-    int n = 7;
+    const int n = 7;
     cout << "Sum of " << n << " elements is: " << GetSum(n);
 }
