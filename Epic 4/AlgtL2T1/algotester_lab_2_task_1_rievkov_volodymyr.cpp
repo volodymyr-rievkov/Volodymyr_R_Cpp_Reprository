f31468a6d6344bbce5172b5f82fee2d4f702d67a
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2023-12-04 21:08:28 +0200

    algotester done

---
+++ Epic 4/AlgtL2T1/algotester_lab_2_task_1_rievkov_volodymyr.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<sstream>
+using namespace std;
+
+int get_size()
+{
+    int size;
+    cin >> size;
+    return size;
+}
+
+void fill_array(int* array, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+}
+
+void print_array(int* array, int size)
+{
+    cout << "Array[" << size << "] = {";
+    for(int i = 0; i < size; i++)
+    {
+        cout << array[i];
+        if(i != size - 1)
+        {
+            cout << ", ";
+        }
+    }
+    cout << "}" << endl;
+}
+
+int get_max_array_number(int* array, int size)
+{
+    int max = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int get_min_array_number(int* array, int size, int max)
+{
+    int min = max;
+    for(int i = 0; i < size; i++)
+    {
+        if(array[i] < min && array[i] != -1)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int get_count_of_array_number(int* array, int size, int number)
+{
+    int count = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if(number == array[i])
+        {
+            count++;
+        } 
+    }
+    return count;    
+}
+
+void delete_array_number(int* array, int size, int number)
+{
+    for(int i = 0; i < size; i++)
+    {
+        if(array[i] == number)
+        {
+            array[i] = -1;
+        }
+    }
+}
+
+int get_result(int* array, int size)
+{
+    int max = get_max_array_number(array, size);
+    int min = get_min_array_number(array, size, max);
+    int max_count = get_count_of_array_number(array, size, max);
+    int min_count = get_count_of_array_number(array, size, min);
+    if(max_count == min_count)
+    {
+        delete_array_number(array, size, max);
+    }
+    else
+    {
+        if(max_count < min_count)
+        {
+            delete_array_number(array, size, max);
+        }
+        else
+        {
+            delete_array_number(array, size, min);
+        }
+    }
+    max = get_max_array_number(array, size);
+    min = get_min_array_number(array, size, max);
+    return  max - min;
+}
+
+int main()
+{
+    int size = get_size();
+    int a_road[size];
+    fill_array(a_road, size);
+    cout << get_result(a_road, size);
+}   
